82218a517cf9f93502ebe63fe01a0066a0e58980
Laurent Gougeon <syi@ecmwf.int>
2024-09-06 17:46:59 +0200

    Moved jni library building direct from maven.

--- src/main/c/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -x
-gcc -shared -O2 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -o libsocketoptions.so \
-	-I/usr/lib/jvm/graalvm-community-openjdk-22.0.1+8.1/include \
-	-I/usr/lib/jvm/graalvm-community-openjdk-22.0.1+8.1/include/linux \
-	-fPIC SocketOptions.c
