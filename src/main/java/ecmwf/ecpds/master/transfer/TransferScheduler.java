c3bac6cae689cdf1ade51b40224285a119da78f8
Laurent Gougeon <syi@ecmwf.int>
2025-07-01 10:53:33 +0200

    Removed unused import.

--- src/main/java/ecmwf/ecpds/master/transfer/TransferScheduler.java
+++ src/main/java/ecmwf/ecpds/master/transfer/TransferScheduler.java
@@ -50,7 +50,6 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
