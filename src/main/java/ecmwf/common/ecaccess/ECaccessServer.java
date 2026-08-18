2a6dcb490474feb817a571a66311c843a5e13c3a
Laurent Gougeon <laurent.gougeon@ecmwf.int>
2026-08-18 17:14:38 +0200

    Migration from Commons Lang to Commons Lang v3.

--- src/main/java/ecmwf/common/ecaccess/ECaccessServer.java
+++ src/main/java/ecmwf/common/ecaccess/ECaccessServer.java
@@ -43,7 +43,7 @@ import javax.management.MalformedObjectNameException;
 import javax.management.NotCompliantMBeanException;
 import javax.management.timer.Timer;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
